6068e1a24078e56298c1d03dbb67b04eb332d9d9
Anders <mail@andersandersson.net>
2020-03-02 22:36:50 +0100

    Trying stuff

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
+
+import Start from './pages/Start/Start.js';
+
 const app = () => {
 
     const world = "WORLD";
+    let hej = "HEJHEJ";
+    const yo = () => {
+        return `
+        hej = 1337;
+        alert(hej)
+        `;
+    }
+    return(`hej <div onClick="${yo()}">${hej}</div>`)
 
-
-    return (`<h1>Hello ${world}!</h1>`);
+    //return ();
+    //  return (`${Start()}`);
 
 };
 
---
+++ src/pages/Start/Start.js
@@ -0,0 +1,22 @@
+const Start = () => {
+    const style = {
+        Start: `
+            width:100%;
+            height: 100px; 
+            background: linear-gradient(to right, #ef32d9, #89fffd);
+            color: white;`,
+    };
+
+
+    return (`
+        <div style="${style.Start}">
+            <h1>Välkommen till startsidan1</h1>
+            Hej och bvälkommen;
+        </div>
+    `);
+
+
+
+}
+
+export default Start;
