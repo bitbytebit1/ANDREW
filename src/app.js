34cf833b5254edef68848626e85afcd99c8612f2
bitbytebit1 <tristan@tribe.nu>
2020-03-02 23:10:28 +0100

    Update app.js
    memes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
-const app = () => {
-
-    const world = "WORLD";
-
-
-    return (`<h1>Hello ${world}!</h1>`);
+const app = (sHelloWorldString) => {
+    
+    if (!sHelloWorldString) {
+        const world = "WORLD";
+        return (`<h1>Hello ${world}!</h1>`);
+    } else if (sHelloWorldString) {
+        return (`<h1>${sHelloWorldString}!</h1>`);
+    }
 
 };
 
